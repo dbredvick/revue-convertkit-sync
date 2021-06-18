ea497dec3085960a0c3b46288f974eafdaa0eef7
Drew Bredvick <dbredvick@gmail.com>
2021-06-18 13:22:41 -0500

    feat: add logo

--- theme.config.js
+++ theme.config.js
@@ -14,22 +14,7 @@ export default {
   footerEditOnGitHubLink: true, // will link to the docs repo
   logo: (
     <>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="24"
-        width="24"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-        />
-      </svg>
-      <span style={{ marginLeft: "4px" }}>Revue ConvertKit Sync</span>
+      <img width="300px" src="/revue-convertkit-sync.svg" />
     </>
   ),
   head: (
